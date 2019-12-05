28cd8f45dd1172bd7bbddd187bfe75cb6fd207ab
Markus Meingast <markus.meingast@mail.mcgill.ca>
2019-12-05 19:18:10 +0100

    Started building U-Net model

--- trainval/train.py
+++ trainval/train.py
@@ -19,8 +19,8 @@ import matplotlib.pyplot as mp
 # %% LOAD DATA AND SPLIT
 ################################################################################
 
-X = np.load('pre/image-0.npy')
-y = np.load('pre/label-0.npy')
+X = np.load('pre/image-0.npy')[0:400]
+y = np.load('pre/label-0.npy')[0:400]
 X_train, X_test, y_train, y_test = train_test_split(X,y)
 
 ################################################################################
@@ -37,12 +37,18 @@ X_test = X_test/255.0
 ##### GET INPUT SHAPE
 (_, xdim, ydim, zdim) = X_train.shape
 
+### https://github.com/zhixuhao/unet/blob/master/model.py
 ##### ADD LAYERS
 model = keras.Sequential()
 model.add(keras.layers.InputLayer(input_shape=(xdim,ydim,zdim), dtype=tf.float32))
-model.add(keras.layers.Conv2D(4, (3, 3), padding='same'))
-model.add(keras.layers.Conv2D(10, (3, 3), activation='relu', padding='same'))
-model.add(keras.layers.Conv2D(1, (3, 3), activation='relu', padding='same'))
+model.add(keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))
+model.add(keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))
+#model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))
+
+
+#model.add(keras.layers.Conv2D(10, (3, 3), activation='relu', padding='same'))
+model.add(keras.layers.Conv2D(1, (1, 1), activation='relu', padding='same'))
 
 ##### COMPILE MODEL AND PRINT SUMMARY
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -64,7 +70,7 @@ earlystopping = EarlyStopping(monitor='val_loss', patience=3)
 history = model.fit(
     x=X_train,
     y=y_train,
-    epochs=1,
+    epochs=10,
     verbose=1,
     validation_data=(X_test, y_test),
     use_multiprocessing=True,
@@ -76,7 +82,7 @@ history = model.fit(
 # %% PLOTTING
 ################################################################################
 
-id = 16
+id = 23
 
 mp.imshow(X_train[id,:,:,:])
 mp.imshow(y_train[id,:,:,0], alpha=0.5)
@@ -86,16 +92,10 @@ mp.show()
 # %% PLOTTING
 ################################################################################
 
-X_train.dtype
-
 X_pred = X_train[id]
 X_pred = X_pred[np.newaxis]
 y_pred = model.predict(X_pred)
 
-
-
 mp.imshow(X_pred[0])
-mp.imshow(y_pred[0,:,:,0].round(), alpha=0.9)
+mp.imshow(y_pred[0,:,:,0]> 0.01, alpha=0.9)
 mp.show()
-
-y_pred.max()
